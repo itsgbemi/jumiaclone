bd6a72093f8bf786470ea6cd125cb9505440f9a7
Gbemisola Oyeniyi <117040187+itsgbemi@users.noreply.github.com>
2025-10-22 14:14:25 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+content: [
+"./pages/**/*.{js,ts,jsx,tsx}",
+"./components/**/*.{js,ts,jsx,tsx}",
+],
+theme: {
+extend: {
+fontFamily: {
+sans: ['Inter', 'sans-serif'],
+},
+colors: {
+accent: '#f6912c',
+},
+},
+},
+plugins: [],
+}
